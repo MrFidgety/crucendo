d22ed3700932fda17a4cbc0e1bf495044a6c000c
Tristan Hart <tristan@thecrucialteam.com>
2016-07-13 02:32:31 +0000

    Update staging host to bugs.thecrucialteam.com

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  host = 'http://archer.thecrucialteam.com';
+  host = 'http://bugs.thecrucialteam.com';
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
      :authentication        => :plain,
