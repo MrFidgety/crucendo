bf16d5c22b15cf64bc52cc735d633053a74831c1
Tristan Hart <tristan@thecrucialteam.com>
2016-08-01 06:12:38 +0000

    Remove GA variable from environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  # Google Analytics
-  GA.tracker = "UA-74384967-1"
 end
