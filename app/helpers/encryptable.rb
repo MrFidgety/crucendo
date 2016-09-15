55c5d3d66db344d7bc22675ea756010455525592
Tristan Hart <tristan@thecrucialteam.com>
2016-09-15 07:03:01 +0000

    Modify crypt logic

--- app/helpers/encryptable.rb
+++ app/helpers/encryptable.rb
@@ -21,9 +21,12 @@ module Encryptable
 
   def crypt
     @crypt ||= begin
-      key = ActiveSupport::KeyGenerator.new(
-              Rails.application.secrets.answer_key).generate_key(
-              Rails.application.secrets.answer_salt)    
+      case self
+      when Answer.class
+        env_key = Rails.application.secrets.answer_key
+        env_salt = Rails.application.secrets.answer_salt
+      end
+      key = ActiveSupport::KeyGenerator.new(env_key).generate_key(env_salt)    
       ActiveSupport::MessageEncryptor.new(key)
     end
   end
