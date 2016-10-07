c86904f31d8f7eb90e10a8d305a6ad6abb8c22d0
Tristan Hart <tristan@thecrucialteam.com>
2016-10-07 07:44:49 +0000

    slight update to subs page, wording and panel smaller on mobile

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,7 +2,7 @@ class SubscriptionsController < ApplicationController
   before_action :logged_in_user
   
   def index
-    @subscriptions = Topic.active
+    @subscriptions = Topic.active.order(name: :asc)
   end
   
   def update
