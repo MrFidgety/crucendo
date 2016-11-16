9e917be53d5306bd1b469c4e337540e118469be3
Tristan Hart <tristan@thecrucialteam.com>
2016-11-16 06:38:00 +0000

    ServiceWorker again

--- app/assets/javascripts/serviceworker.js
+++ app/assets/javascripts/serviceworker.js
@@ -1,9 +1,11 @@
+var version = 'v1::';
+
 function onInstall(event) {
   console.log('[Serviceworker]', "Installing!");
   event.waitUntil(
-    caches.open('cached-assets-v1').then(function prefill(cache) {
+    caches.open(version + 'offline').then(function prefill(cache) {
       return cache.addAll([
-        '/offline.html',
+        '/offline.html'
       ]);
     })
   );
@@ -11,22 +13,31 @@ function onInstall(event) {
 
 function onActivate() {
   console.log('[Serviceworker]', "Activating!");
+  event.waitUntil(
+    caches.keys().then(function deleteOldCache(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.indexOf(version) !== 0;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
 }
 
 function onFetch(event) {
-  // Fetch from network, fallback to cached content, then offline.html for same-origin GET requests
   var request = event.request;
 
   //if (!request.url.match(/^https?:\/\/example.com/) ) { return; }
   if (request.method !== 'GET') { return; }
 
   event.respondWith(
-    fetch(request)                                       // first, the network
-      .catch(function fallback() {
-         caches.match(request).then(function(response) {  // then, the cache
-           response || caches.match("/offline.html");     // then, /offline cache
-         })
+    fetch(request).catch(function fallback() {
+       caches.match(request).then(function(response) {
+         response || caches.match("/offline.html");
        })
+     })
   );
 }
 
